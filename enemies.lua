60d1e99206888e10f17ef4649eb4765b21db4ac3
Robert Woods <robertjawoods@gmail.com>
2016-01-15 12:33:07 +0000

    Restructured enemies to be object oriented

---
+++ enemies.lua
@@ -0,0 +1,38 @@
+local Enemies = { img = nil, enemies = {}, createEnemyTimerMax = 0.9, createEnemyTimer = 0, deathSound = nil }
+
+function Enemies:Initialise()
+  Enemies.createEnemyTimerMax = 0.9
+  Enemies.createEnemyTimer = Enemies.createEnemyTimerMax
+  Enemies.img = love.graphics.newImage('assets/enemy.png')
+  Enemies.deathSound = love.audio.newSource('assets/explosion.wav', 'stream')
+end
+
+function Enemies:Create(dt)
+  Enemies.createEnemyTimer = Enemies.createEnemyTimer - (1 * dt)
+
+  if Enemies.createEnemyTimer < 0 then
+    Enemies.createEnemyTimer = Enemies.createEnemyTimerMax
+
+    local randomNumber = love.math.random(10, love.graphics.getWidth() - 10)
+    newEnemy = { x = randomNumber, y = -10, img = Enemies.img }
+    table.insert(Enemies.enemies, newEnemy)
+  end
+end
+
+function Enemies:Move(dt)
+  for i, enemy in ipairs(Enemies.enemies) do
+    enemy.y = enemy.y + (140 * dt)
+
+    if enemy.y > 650 then
+      table.remove(Enemies.enemies, i)
+    end
+  end
+end
+
+function Enemies:Draw()
+  for i, enemy in ipairs(enemies) do
+      love.graphics.draw(enemy.img, enemy.x, enemy.y)
+  end
+end
+
+return Enemies
