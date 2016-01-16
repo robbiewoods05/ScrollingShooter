6e8f814f6bbf642a7ed7fc8c30ea7d7d48a99219
Robert Woods <robertjawoods@gmail.com>
2016-01-16 15:40:11 +0000

    Added death animation

--- main.lua
+++ main.lua
@@ -5,10 +5,13 @@ Enemies = require "src/enemies"
 Bullets = require "src/bullets"
 Animation = require "src/Animation"
 
+
 background = nil
 backgroundMusic = nil
 
 local explosionAnimation = nil
+
+frame = 1
 
 function CheckCollision(x1, y1, w1, h1, x2, y2, w2, h2)
   return x1 < x2 + w2 and
@@ -27,76 +30,89 @@ function love.load(arg)
   backgroundMusic = love.audio.newSource('assets/bgmusic.ogg', 'stream')
 
   explosionAnimation = Animation:CreateQuads(96, 96, 3, 5, explosionSpritesheet)
+
 end
 
 function love.update(dt)
     love.audio.play(backgroundMusic)
+    if not Player.isAlive then
+      Animation:Update(dt)
+    end
   if love.keyboard.isDown('escape') then
     love.event.push('quit')
   end
 
   if not isAlive and love.keyboard.isDown('r') then
-      Player:Initialise()
-  end
+     Player:Initialise()
+     Animation.Reset()
+ end
 
-  --Movement
-  if love.keyboard.isDown('left', 'a') and Player.x > 0 then
-    Player:Move("left", dt)
-  elseif love.keyboard.isDown('right', 'd') and Player.x  < love.graphics.getWidth() - Player.img:getWidth() then
-    Player:Move("right", dt)
-  elseif love.keyboard.isDown('up', 'w') and Player.y > 0 then
-    Player:Move("up", dt)
-  elseif love.keyboard.isDown('down', 's') and Player.y < love.graphics.getHeight() - Player.img:getHeight() then
-    Player:Move("down", dt)
-  end
+ --Movement
+ if love.keyboard.isDown('left', 'a') and Player.x > 0 then
+   Player:Move("left", dt)
+ elseif love.keyboard.isDown('right', 'd') and Player.x  < love.graphics.getWidth() - Player.img:getWidth() then
+   Player:Move("right", dt)
+ elseif love.keyboard.isDown('up', 'w') and Player.y > 0 then
+   Player:Move("up", dt)
+ elseif love.keyboard.isDown('down', 's') and Player.y < love.graphics.getHeight() - Player.img:getHeight() then
+   Player:Move("down", dt)
+ end
 
-  Player.canShootTimer = Player.canShootTimer - (1 * dt)
-  if Player.canShootTimer < 0 then
-    Player.canShoot = true
-  end
+ if love.keyboard.isDown('e') then
+   Player.isAlive = false
+ end
 
-  --Shooting
-  if love.keyboard.isDown('space', 'lctrl', 'rctrl') and Player.canShoot and Player.isAlive then
-    love.audio.play(Player.shootSound)
-    Bullets:Create()
-    Player.canShoot = false
-    Player.canShootTimer = Player.canShootTimerMax
-  end
+ Player.canShootTimer = Player.canShootTimer - (1 * dt)
+ if Player.canShootTimer < 0 then
+   Player.canShoot = true
+ end
 
-  Player:Fire(dt)
+ --Shooting
+ if love.keyboard.isDown('space', 'lctrl', 'rctrl') and Player.canShoot and Player.isAlive then
+   love.audio.play(Player.shootSound)
+   Bullets:Create()
+   Player.canShoot = false
+   Player.canShootTimer = Player.canShootTimerMax
+ end
 
-  Enemies:Create(dt)
-  Enemies:Move(dt)
+ Player:Fire(dt)
 
-  -- Check for collision between enemy and bullet
-  for i, enemy in ipairs(Enemies.enemies) do
-    for j, bullet in ipairs(Bullets.bullets) do
-      if CheckCollision(enemy.x, enemy.y, Enemies.img:getWidth(), Enemies.img:getHeight(), bullet.x, bullet.y, Bullets.img:getWidth(), Bullets.img:getHeight()) then
-        table.remove(Bullets.bullets, j)
-        table.remove(Enemies.enemies, i)
-        love.audio.play(Enemies.deathSound)
-        Player.score = Player.score + 10
-      end
-    end
+ Enemies:Create(dt)
+ Enemies:Move(dt)
 
-    -- Check for collision between enemy and player
-    if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), Player.x, Player.y, Player.img:getWidth(), Player.img:getHeight()) and Player.isAlive then
-      table.remove(Enemies.enemies, i)
-      Player.health = Player.health - 10
-      love.audio.play(Enemies.deathSound)
-      if Player.health == 0 then
-          Player:Die()
-      end
-    end
-  end
+ -- Check for collision between enemy and bullet
+ for i, enemy in ipairs(Enemies.enemies) do
+   for j, bullet in ipairs(Bullets.bullets) do
+     if CheckCollision(enemy.x, enemy.y, Enemies.img:getWidth(), Enemies.img:getHeight(), bullet.x, bullet.y, Bullets.img:getWidth(), Bullets.img:getHeight()) then
+       table.remove(Bullets.bullets, j)
+       table.remove(Enemies.enemies, i)
+       love.audio.play(Enemies.deathSound)
+       Player.score = Player.score + 10
+     end
+   end
 
-function love.draw()
+   -- Check for collision between enemy and player
+   if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), Player.x, Player.y, Player.img:getWidth(), Player.img:getHeight()) and Player.isAlive then
+     table.remove(Enemies.enemies, i)
+     Player.health = Player.health - 10
+     love.audio.play(Enemies.deathSound)
+     if Player.health == 0 then
+         Player:Die()
+     end
+   end
+ end
+end
+
+function love.draw(dt)
     love.graphics.draw(background, 0, 0)
     if Player.isAlive then
         love.graphics.print("Score: " .. Player.score)
-        love.graphics.print("Health remaining: " .. Player.health, love.graphics.getWidth() - 120, 0)
+        love.graphics.print("Health remaining: " .. Player.health, love.graphics.getWidth() - 140, 0)
 
         --Debug
+        if Animation.frame ~= nil then
+          love.graphics.print("Frame: " .. Animation.frame, 0, 387)
+        end
         love.graphics.print("FPS: " .. love.timer.getFPS(), 0, 400)
         love.graphics.print("Delta Time: " .. string.format("%4.3f", love.timer.getDelta()), 0 , 412)
         love.graphics.print("Enemies: " .. #Enemies.enemies, 0, 427)
@@ -105,6 +121,7 @@ function love.draw()
         love.graphics.print("Left: " .. tostring(love.keyboard.isDown("a", "left")), 0, 472)
         love.graphics.print("Right: " .. tostring(love.keyboard.isDown("d", "right")), 0, 487)
 
+
         love.graphics.draw(Player.img, Player.x, Player.y)
 
         Bullets:Draw()
@@ -112,8 +129,6 @@ function love.draw()
         Enemies:Draw()
 
     else
-        Animation:PlayAnimation(explosionAnimation, Player.x, Player.y, explosionSpritesheet)
-        love.graphics.printf("You are dead. Press R to restart.", 170, (love.graphics.getHeight()/2), 500, "center")
+        Animation:PlayAnimation(explosionSpritesheet, Player.x, Player.y)
     end
-end
 end
--- src/Animation.lua
+++ src/Animation.lua
@@ -1,28 +1,49 @@
 local Animation = {}
 
+timer=0
+frame = 1
+frames = 15
+
+
 function Animation:CreateQuads(spriteWidth, spriteHeight, rows, columns, img)
-  local frame = 1
-  local Animation = {}
+  local index = 1
+  Animations = {}
   local xPos = 0
   local yPos = 0
 
   for _ = 1, rows do
     for __ = 1, columns do
-      Animation[frame] = love.graphics.newQuad(xPos, yPos, spriteWidth, spriteHeight, img:getWidth(), img:getHeight())
+      Animation[index] = love.graphics.newQuad(xPos, yPos, spriteWidth, spriteHeight, img:getWidth(), img:getHeight())
       xPos = xPos + spriteWidth
-      frame = frame + 1
+      index = index + 1
     end
     xPos = 0
     yPos = yPos + spriteHeight
   end
-
-  return Animation
 end
 
-function Animation:PlayAnimation(animationArray, x, y, img)
-  for frame = 1, #animationArray do
-    love.graphics.draw(img, animationArray[frame], x, y)
+function Animation:Update(dt)
+  timer = timer - dt
+
+  if timer < 0 and frame ~= 15 then
+    frame = (frame % frames) + 1
+    timer = timer + 0.13
   end
+
+
+end
+
+function Animation:PlayAnimation(img, x, y)
+    love.graphics.draw(img, Animation[frame], x, y)
+    if frame == 15 then
+      love.graphics.printf("You are dead. Press R to restart.", 170, (love.graphics.getHeight()/2), 500, "center")
+    end
+end
+
+function Animation.Reset()
+  timer = 0
+  frame = 1
+  frames = 15
 end
 
 return Animation
